2edccd5d1d6237fbe2cf791d9cc4dd597bdd06f2
GeroZayas <gerozayas@gmail.com>
2022-04-14 13:29:21 +0200

    Update

--- 25_Build_A_Pile_Of_Cubes.js
+++ 25_Build_A_Pile_Of_Cubes.js
@@ -3,7 +3,7 @@ Your task is to construct a building which will be a pile of n cubes. The cube a
 
 You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
 
-The parameter of the function findNb (find_nb, find-nb, findNb, ...) will be an integer m and you have to return the integer n such as n^3 + (n-1)^3 + ... + 1^3 = m if such a n exists or -1 if there is no such n.
+The parameter of the function findNb will be an integer m and you have to return the integer n such as n^3 + (n-1)^3 + ... + 1^3 = m if such a n exists or -1 if there is no such n.
 
 Examples:
 findNb(1071225) --> 45
@@ -12,15 +12,13 @@ findNb(91716553919377) --> -1
 */
 
 function findNb(m) {
-  var result = 1;
-  var cubes = 0;
-
-  while (result <= m) {
-    result += cubes ** 3;
-    cubes++;
+  let total = 0;
+  let n = 0;
+  while (total < m) {
+    total += Math.pow(n, 3);
+    n++;
   }
-  return cubes;
-  return -1;
+  return total === m ? n - 1 : -1;
 }
 
 console.log(findNb(1071225)); // 45
