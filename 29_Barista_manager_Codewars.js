7f611f98fae042950b067c489223030b287230b7
GeroZayas <gerozayas@gmail.com>
2022-04-16 15:55:51 +0200

    Update

--- 29_Barista_manager_Codewars.js
+++ 29_Barista_manager_Codewars.js
@@ -10,17 +10,20 @@ function barista(coffees, cMachines) {
     }
     coffees = newCoffees;
     // --------------------------------------------------------
-    console.log(coffees);
+    // /////////// ////// Sort the array
     // --------------------------------------------------------
-    // Sort the array
+    // this is to order the array from min to max
+    coffees = coffees.sort(function (a, b) {
+      return a - b;
+    });
     // --------------------------------------------------------
-
+    // /////////// ////// Divide into arrays
     // --------------------------------------------------------
   }
   return 0;
 }
 
-console.log(barista([0, 0, 1], 1)); // 1
-// console.log(barista([2, 3, 4], 1)); // 22
+// console.log(barista([0, 0, 1], 1)); // 1
+console.log(barista([4, 3, 2], 1)); // 22
 // console.log(barista([2, 3, 4], 2)); // 13
 // console.log(barista([2, 3, 4], 3)); // 9
