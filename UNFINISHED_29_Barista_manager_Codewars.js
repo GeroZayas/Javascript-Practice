35b0989274bb8cdd9f2dde99a798e074587020f4
GeroZayas <gerozayas@gmail.com>
2022-04-21 23:40:24 +0200

    Update

---
+++ UNFINISHED_29_Barista_manager_Codewars.js
@@ -0,0 +1,8 @@
+function barista(coffees, cMachines) {
+  return "Let's code!";
+}
+
+console.log(barista([0, 0, 1], 1)); // 1
+console.log(barista([4, 3, 2], 1)); // 22
+console.log(barista([2, 3, 4], 2)); // 13
+console.log(barista([2, 3, 4], 3)); // 9
