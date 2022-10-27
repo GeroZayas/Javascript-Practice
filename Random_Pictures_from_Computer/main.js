f724453deaa9c7252ac52cada1c6892ccd0bb1d9
GeroZayas <gerozayas@gmail.com>
2022-10-27 23:24:29 +0200

    Initial

---
+++ Random_Pictures_from_Computer/main.js
@@ -0,0 +1,11 @@
+// select button
+
+const generateBtn = document.getElementById("generateBtn");
+
+function hello() {
+  console.log("HELLO");
+}
+
+//  fuction that shows
+
+generateBtn.addEventListener("click", hello);
