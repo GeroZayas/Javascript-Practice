3e531277c985c5983a40cfb9e82a722c63f18d8e
GeroZayas <gerozayas@gmail.com>
2022-12-19 00:10:02 +0100

    Update

--- Random_Pictures_from_Computer/main.js
+++ Random_Pictures_from_Computer/main.js
@@ -1,7 +1,1 @@
-var path = "/images/PinDown__Quotes/",
-  imgs = ["1.jpg", "2.jpg", "3.jpg"],
-  i = Math.floor(Math.random() * imgs.length);
-$(".image")
-  .append("<img src='" + path + imgs[i] + "'>")
-  .hide()
-  .fadeIn(2000);
+
